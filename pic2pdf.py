82781af60c439c2633177525c4cb6e485367907e
Carter Tsai <hamming1@gmail.com>
2013-09-05 00:41:14 +0800

    Draw picture form and Add gitignore

--- pic2pdf.py
+++ pic2pdf.py
@@ -4,19 +4,23 @@ from reportlab.pdfgen import canvas
 pdfWidht = 595
 pdfHeight = 842
 
+pitch = 2
 picWidth = (595 / 2) - (10 * 2)
-picHeight = 280
+picHeight = 270
 picInteval = 15
+
 
 # create canvas
 c = canvas.Canvas("picture.pdf")
 
 imageLen = 6
-#for v in range(6):
-#c.rect(0 + (v * 100), 750, picWidth, picHeight)
-c.rect(0 + picInteval, 500, picWidth, picHeight)
-c.rect(306, 500, picWidth, picHeight)
-
+picX = 0
+picY = 560
+for v in range(1, imageLen + 1):
+    c.rect(picX + picInteval + ((v % 2) * picWidth), picY, picWidth, picHeight)
+    if((v % 2) == 0):
+        picY -= picHeight
+        picX = 0
 
 c.drawString(100, 750, "Welcome to Reportlab!")
 
