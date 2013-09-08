d21e4f4a154bbe27989fc938846a2321688795ec
Carter Tsai <hamming1@gmail.com>
2013-09-08 22:19:53 +0800

    Add images to pdf, but has bugs

--- pic2pdf.py
+++ pic2pdf.py
@@ -1,4 +1,5 @@
 from reportlab.pdfgen import canvas
+import glob
 
 # A4
 pdfWidht = 595
@@ -9,20 +10,26 @@ picWidth = (595 / 2) - (10 * 2)
 picHeight = 270
 picInteval = 15
 
+# get all of picture from
+imgs = glob.glob('./img/*')
+imageLen = len(imgs)
 
 # create canvas
 c = canvas.Canvas("picture.pdf")
 
-imageLen = 6
 picX = 0
 picY = 560
+
+# draw form
 for v in range(1, imageLen + 1):
-    c.rect(picX + picInteval + ((v % 2) * picWidth), picY, picWidth, picHeight)
+    offsetX = picX + picInteval + ((v % 2) * picWidth)
+    offsetY = picY
+    c.rect(offsetX, offsetY, picWidth, picHeight)
+    # draw image
+    c.drawImage(imgs[v-1], offsetX, offsetY, picWidth, picHeight)
     if((v % 2) == 0):
         picY -= picHeight
         picX = 0
 
 c.drawString(100, 750, "Welcome to Reportlab!")
-
-
 c.save()
